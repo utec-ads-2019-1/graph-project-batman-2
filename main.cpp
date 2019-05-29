5f181425c15aa46920c2895816493135db8f7b0c
unknown <mauricio.pinto@utec.edu.pe>
2019-05-28 21:18:15 -0500

    primKruskal

--- main.cpp
+++ main.cpp
@@ -6,6 +6,15 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+    graph g;
 
+    g.addNode('A');
+    g.addNode('B');
+    g.addNode('C');
+    g.addNode('D');
+
+    g.addEdge(5, 'A', 'B');
+    g.addEdge(7, 'B', 'C');
+    g.addEdge(9, 'C', 'D');
     return EXIT_SUCCESS;
 }
