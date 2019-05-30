cd58dfbc6aaebca78ac262e5b3423a68f4badeca
L4mb0 <juan.navarro@utec.edu.pe>
2019-05-29 20:48:13 -0500

    destructor, conectividad, densidad, fullprint y medio dfs

--- main.cpp
+++ main.cpp
@@ -12,10 +12,15 @@ int main(int argc, char *argv[]) {
     g.addNode('B');
     g.addNode('C');
     g.addNode('D');
+    g.addNode('E');
 
     g.addEdge(5, 'A', 'B');
     g.addEdge(7, 'B', 'C');
-    g.addEdge(9, 'C', 'D');
+    g.addEdge(9, 'C', 'E');
+    g.addEdge(9, 'E', 'B');
+    g.addEdge(9, 'A', 'D');
+    g.addEdge(9, 'D', 'B');
 
+    g.DFS('B');
     return EXIT_SUCCESS;
 }
